8c056aa0ca5c8cbf022490bd1a5da64904481dc6
Ram Iyer <ramky.iyer@yahoo.com>
2014-08-12 13:41:53 -0700

    completed integrating api tests

---
+++ spec/models/stripe_wrapper_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe StripeWrapper::Charge do
+  before do
+    StripeWrapper.set_api_key
+  end
+
+  let(:token) do
+    Stripe::Token.create(
+      :card => {
+        number: card_number,
+        exp_month: 8,
+        exp_year: 2018,
+        cvc: 123
+      }
+    ).id
+  end
+
+  context "with valid card" do
+    let(:card_number) { '4242424242424242' }
+    let(:response){
+      StripeWrapper::Charge.create(amount: 300, card: token)
+    }
+    it "charges the card successfully" do
+       response.should be_successful
+     end
+  end
+
+  context "with invalid card" do
+    let(:card_number) { '4000000000000002' }
+    let(:response){
+      StripeWrapper::Charge.create(amount: 300, card: token)
+    }
+
+    it "does not charge the card successfully" do
+      response.should_not be_successful
+    end
+
+    it "contains an error message" do
+      response.error_message.should == 'Your card was declined.'
+    end
+  end
+end
